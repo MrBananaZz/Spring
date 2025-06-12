fc109d79d505c06ee6e64627dba2abc9a1dd4549
MrBananaZz <elilacki@gmail.com>
2025-06-12 13:42:12 +0200

    Added API Dependency and Key

--- src/main/java/at/alt/SoftwareEngineerService.java
+++ src/main/java/at/alt/SoftwareEngineerService.java
@@ -10,28 +10,34 @@ public class SoftwareEngineerService {
 
     private final SoftwareEngineerRepository repository;
 
+    //Constructor
     public SoftwareEngineerService(SoftwareEngineerRepository repository) {
         this.repository = repository;
     }
 
+    //getAll
     public List<SoftwareEngineer> getAllSoftwareEngineers() {
         return repository.findAll();
     }
 
+    //create
     public void insertSoftwareEngineer(SoftwareEngineer softwareEngineer) {
         repository.save(softwareEngineer);
     }
 
+    //findById
     public SoftwareEngineer getAllSoftwareEngineersById(int id) {
         return repository.findById(id)
                 .orElseThrow(() -> new IllegalStateException(id + "not found"));
     }
 
+    //delete
     public void deleteSoftwareEngineerById(int id) {
         repository.deleteById(id);
 
     }
 
+    //update
     public void updateSoftwareEngineerById(int id, SoftwareEngineer update) {
         SoftwareEngineer softwareEngineer = repository.findById(id)
                 .orElseThrow(() -> new IllegalStateException(
