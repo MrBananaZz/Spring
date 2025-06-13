d0e01f10dc608c885071e840f89580359f5159b2
MrBananaZz <elilacki@gmail.com>
2025-06-13 22:00:56 +0200

    implemented logic for JwtService

--- src/main/java/at/alt/service/JwtService.java
+++ src/main/java/at/alt/service/JwtService.java
@@ -1,11 +1,34 @@
 package at.alt.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "a3f1c9e4d72b8f6a9e0d3b7c5f1249a8b6d3e7f1c8a9b0d2e4f6a7c1b3d5e8f";
+
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
