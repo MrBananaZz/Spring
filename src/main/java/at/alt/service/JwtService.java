3e22e556a3817a24592f0022cb99d0b6a9fb7bad
MrBananaZz <elilacki@gmail.com>
2025-06-18 13:43:47 +0200

    finished JwtService

--- src/main/java/at/alt/service/JwtService.java
+++ src/main/java/at/alt/service/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.hibernate.annotations.DialectOverride;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -46,6 +47,20 @@ public class JwtService {
                 .compact();
 
     }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
 
     private Claims extractAllClaims(String token) {
         return Jwts
